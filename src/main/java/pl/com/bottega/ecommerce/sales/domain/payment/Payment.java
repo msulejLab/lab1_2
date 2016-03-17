20a789ec5d95c22dd38a4ccff05f824bc1181748
Maciej Sulej <maciejsulej@gmail.com>
2016-03-17 21:29:11 +0100

    Dodanie PaymentFactory

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/Payment.java
@@ -37,6 +37,6 @@ public class Payment {
 	public Payment rollBack() {
 		Id id = Id.generate();
 
-		return new Payment(id, clientData, amount.multiplyBy(-1));		
+		return PaymentFactory.getPayment(id, clientData, amount.multiplyBy(-1));
 	}
 }
